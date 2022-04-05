d40b2a45379a94a9eb7cde192bf5c9fd72fb6b4e
markpokornycos <markpokornycos@gmail.com>
2022-04-05 12:17:50 -0600

    adds package manifest

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "AsyncNetworkService",
+    products: [
+        .library(
+            name: "AsyncNetworkService",
+            targets: ["AsyncNetworkService"]
+        )
+    ],
+    dependencies: [
+        .package(url: URL(string: "https://github.com/AliSoftware/OHHTTPStubs.git")!, from: "9.0.0")
+    ],
+    targets: [
+        .target(
+            name: "AsyncNetworkService",
+            dependencies: ["OHHTTPStubs"]
+        ),
+        .testTarget(
+            name: "AsyncNetworkServiceTests",
+            dependencies: ["OHHTTPStubs", "OHHTTPStubsSwift"]
+        )
+    ]
+)
