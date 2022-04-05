33587e84d37788be16f93bceb8de774b7caa6c9a
markpokornycos <markpokornycos@gmail.com>
2022-04-05 13:41:29 -0600

    adds iOS & macOS platforms

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "AsyncNetworkService",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
     products: [
         .library(
             name: "AsyncNetworkService",
